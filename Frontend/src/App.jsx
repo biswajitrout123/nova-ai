365f191280511f035f2c299274db2f92c776a100
Biswajit Rout <routhappy9876@gmail.com>
2026-07-14 23:17:06 +0530

    Implement backend authentication and initialize frontend auth module

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider } from 'react-router-dom'
+import { Router } from 'react-router.jsx'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+
+    </div>
+  )
+}
+
+export default App
